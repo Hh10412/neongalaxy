3d57c1b78577f1ec69cb0ee709930dc0a4a4d5af
Hh10412 <tranxuangiabao10412@gmail.com>
2026-03-26 20:03:19 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'neon-galaxy-v5.7.2.1'; // Đổi tên này (v4.6, v4.7...) để kích hoạt cập nhật
+const CACHE_NAME = 'neon-galaxy-v5.7.2.2'; // Đổi tên này (v4.6, v4.7...) để kích hoạt cập nhật
 const urlsToCache = [
     './',
     './index.html',
